4b005e8eb18ba47ddd2560ebe1dc462c8f5f8b08
shoda888 <gekka888@gmail.com>
2019-12-21 17:50:45 +0900

    localにdockerimageが無い時にdockerhubからpullできるように修正

--- SystemPJ/ProcessVoice/AlexaFunc/deploy.sh
+++ SystemPJ/ProcessVoice/AlexaFunc/deploy.sh
@@ -3,5 +3,5 @@ echo "DEPLOY START!!"
 # Input your function name
 funcname="alexaFunctionForTello"
 
-docker run --rm -v "$PWD":/var/task mylambda:latest
+docker run --rm -v "$PWD":/var/task shoda888/local2pylambda:latest
 aws lambda update-function-code --function-name ${funcname} --zip-file fileb://deploy_package.zip --profile syspro
