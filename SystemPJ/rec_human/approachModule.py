74f2b772482bc70c23c6549fd68a482a26b9f60d
Mine <mine.go.0525@gmail.com>
2020-01-15 18:52:19 +0900

    [add] ApproachModule
    人検知，追跡用のクラスを追加
    クラス内の処理の仮組みを作成

---
+++ SystemPJ/rec_human/approachModule.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import time
+import matplotlib.pyplot as plt
+
+
+class Approach:
+
+
+    def __init__(self, drone):
+        self.drone = drone
+
+    
+    def process(self):
+        # self.search
+
+        # self.approach
+
+        for i in range(5):
+            time.sleep(1)
+            img = self.drone.read()
+            plt.imshow(img)
+
+
+
+    
+
+    # def search(self):
+    #     """
+    #     被災者をyoloを用いて発見，bboxを作成するメソッド
+    #     """
+
+    # def approach(self):
+    #     """
+    #     searchメソッドで得たbboxを元に，cv2のトラッキングを用いながら
+    #     被災者に接近するメソッド
+    #     """
