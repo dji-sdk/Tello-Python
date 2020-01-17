c0ee9e455b2b826a66183fd9ae2f7ada35ab619a
Mine <mine.go.0525@gmail.com>
2020-01-17 17:52:24 +0900

    [update] processメソッドの仮組み
    メソッド用にフラグを2つ追加
    処理の流れの仮組みを作成

--- SystemPJ/rec_human/approachModule.py
+++ SystemPJ/rec_human/approachModule.py
@@ -9,17 +9,20 @@ class Approach:
 
     def __init__(self, drone):
         self.drone = drone
+        self.DetectFlag = False
+        self.CloseFlag = False
 
     
     def process(self):
 
-        # droneから画像を取得，表示する
+        
         while True:
+
+            # droneから画像を取得，表示する
             frame = self.drone.read() # 取得した画像がからであれば処理を1つスキップ
             if frame is None or frame.size == 0: 
                 continue
 
-            
             image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) # cv2用にrbgの並びを入れ替え
             small_image = cv2.resize(image, dsize=(480,360)) # 画像をリサイズ
 
@@ -28,17 +31,29 @@ class Approach:
 
 
 
-        # self.search
+            # 前回の処理で人を検知してるかどうかで処理を割り振る
+            if not self.DetectFlag: # 検知していない場合はsearchメソッドで人を探す
+                self.search(small_image) # searchメソッドに画像を渡し，処理を行う
+            elif:
+                self.approach(small_image) # approachメソッドに画像を渡し，処理を行う
 
-        # self.approach
+
+
+            # approachメソッドでCloseFlagが立っていたらメソッドを終了
+            if self.CloseFlag:
+                break
+
+        except( KeyboardInterrupt, SystemExit):    # Ctrl+cが押されたら離脱
+		    print( "SIGINTを検知" )
+
     
 
-    def search(self):
+    def search(self, frame):
         """
         被災者をyoloを用いて発見，bboxを作成するメソッド
         """
         
-    def approach(self):
+    def approach(self, frame):
         """
         searchメソッドで得たbboxを元に，cv2のトラッキングを用いながら
         被災者に接近するメソッド
