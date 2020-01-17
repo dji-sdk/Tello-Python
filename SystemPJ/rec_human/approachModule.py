0753353bb96db4e4aeb3d8d44e0aba97ba20eae9
Mine <mine.go.0525@gmail.com>
2020-01-17 16:51:42 +0900

    [update] カメラ画像の取得，表示
    Approachクラス内でdroneからの画像を取得，表示する機能の追加

--- SystemPJ/rec_human/approachModule.py
+++ SystemPJ/rec_human/approachModule.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import time
-import matplotlib.pyplot as plt
+import cv2
 
 
 class Approach:
@@ -12,24 +12,32 @@ class Approach:
 
     
     def process(self):
+
+        # droneから画像を取得，表示する
+        while True:
+            frame = self.drone.read() # 取得した画像がからであれば処理を1つスキップ
+            if frame is None or frame.size == 0: 
+                continue
+
+            
+            image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) # cv2用にrbgの並びを入れ替え
+            small_image = cv2.resize(image, dsize=(480,360)) # 画像をリサイズ
+
+            cv2.imshow("camera", small_image) # 名称が"camera"のウィンドウに画像を表示
+            cv2.waitKey(5) # よくわからんがこれを入れないと画像が正しく表示されない
+
+
+
         # self.search
 
         # self.approach
-
-        for i in range(5):
-            time.sleep(1)
-            img = self.drone.read()
-            plt.imshow(img)
-
-
-
     
 
     def search(self):
         """
         被災者をyoloを用いて発見，bboxを作成するメソッド
         """
-
+        
     def approach(self):
         """
         searchメソッドで得たbboxを元に，cv2のトラッキングを用いながら
