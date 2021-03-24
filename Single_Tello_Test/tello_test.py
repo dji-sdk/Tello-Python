6384d4e482db2012fee46d0aab2de3d5fd2acd6f
Roberto Minelli <roberto.minelli@usi.ch>
2021-03-24 09:08:56 +0100

    Port Single_Tello_Test to Python 3 using threading.Event() to block until response has been received

--- Single_Tello_Test/tello_test.py
+++ Single_Tello_Test/tello_test.py
@@ -1,9 +1,6 @@
 from tello import Tello
 import sys
-from datetime import datetime
 import time
-
-start_time = str(datetime.now())
 
 file_name = sys.argv[1]
 
@@ -17,16 +14,8 @@ for command in commands:
 
         if command.find('delay') != -1:
             sec = float(command.partition('delay')[2])
-            print 'delay %s' % sec
+            print('delay %s' % sec)
             time.sleep(sec)
             pass
         else:
             tello.send_command(command)
-
-log = tello.get_log()
-
-out = open('log/' + start_time + '.txt', 'w')
-for stat in log:
-    stat.print_stats()
-    str = stat.return_stats()
-    out.write(str)
